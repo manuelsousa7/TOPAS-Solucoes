d642d516db1be0a5de6d893a7dacc661453a024b
Manuel Sousa <manuelsousamvs@gmail.com>
2015-05-21 22:48:29 +0100

    Create A_Accepted.cpp

---
+++ TOPAS-Treino/2005/A_Accepted.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define lli long long int
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    cout << a+a+b+b << endl;
+	return 0;
+}
