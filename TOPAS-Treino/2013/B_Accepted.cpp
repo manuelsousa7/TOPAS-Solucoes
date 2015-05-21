16b948a9f50ec870a0911e317e9c96116a1a59d7
Manuel Sousa <manuelsousamvs@gmail.com>
2015-05-21 23:16:34 +0100

    Novos Problemas

--- TOPAS-Treino/2013/B_Accepted.cpp
+++ TOPAS-Treino/2013/B_Accepted.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
-#include <cstring>
-#include <cmath>
-#include <algorithm>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#define lli long long int
-
+#define MAX 1000
 using namespace std;
-
 
 int main()
 {
-
+    int n;
+    cin >> n;
+    char strings[n][MAX];
+    for(int i=0;i<n;i++)
+    cin >> strings[i];
+    for(int i=0;i<n;i++)
+    {
+        for(int ii=0;ii<strings[i][1]-'0';ii++)
+        {
+            cout << strings[i][0];
+        }
+        cout << endl;
+    }
     return 0;
 }
